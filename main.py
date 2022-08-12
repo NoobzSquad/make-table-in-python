cbfd525c173f7232927b431917cafc177004de6a
NoobzSquad <71183209+NoobzSquad@users.noreply.github.com>
2022-08-12 23:54:34 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-print("Welcome to Table Generator in Python")
+print("Welcome to Multiplication Table Generator in Python")
 print("By NoobzSquad")
 
-r = int(input("Enter Your Number: "))
-for x in range(1,11):
-    print(r * x)
+num = int(input("Enter Your Number: "))
+rng = int(input("Enter Your Range: "))
+rng += 1
+print("Multiplication Table Of", num)
+for x in range(1, rng):
+    print(num, 'x', x, '=', num * x)
